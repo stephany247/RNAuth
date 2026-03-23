33838a65c19fee2b700df3e0063ecccca509c654
Stephanie Oguocha <stephanieoguocha@gmail.com>
2026-03-23 16:36:44 +0100

    feat: implement signup functionality

--- util/auth.js
+++ util/auth.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const API_KEY = 'AIzaSyDPKm855u2IjD7OAt3v7gKR599dO08Vapo';
 
-async function createUser(email, password) {
+export async function createUser(email, password) {
   const response = await axios.post(
     'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=' + API_KEY,
     {
