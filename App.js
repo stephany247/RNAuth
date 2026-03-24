5f1e4105865d9883f97f2be11e01648a4a13c09e
Stephanie Oguocha <stephanieoguocha@gmail.com>
2026-03-24 12:55:21 +0100

    feat: persist auth tokens and auto-login users on app start

--- App.js
+++ App.js
@@ -7,10 +7,13 @@ import SignupScreen from './screens/SignupScreen';
 import WelcomeScreen from './screens/WelcomeScreen';
 import { Colors } from './constants/styles';
 import AuthContextProvider, { AuthContext } from './store/auth-context';
-import { useContext } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import IconButton from './components/UI/IconButton';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import * as SplashScreen from 'expo-splash-screen';
 
 const Stack = createNativeStackNavigator();
+SplashScreen.preventAutoHideAsync();
 
 function AuthStack() {
   return (
@@ -66,12 +69,37 @@ function Navigation() {
   );
 }
 
+function Root() {
+  const authCtx = useContext(AuthContext);
+  const [istryingLogin, setIstryingLogin] = useState(true);
+
+  useEffect(() => {
+    async function fetchToken() {
+      const storedToken = await AsyncStorage.getItem('token');
+
+      if (storedToken) {
+        authCtx.authenticate(storedToken);
+      }
+      setIstryingLogin(false);
+      await SplashScreen.hideAsync();
+    }
+
+    fetchToken();
+  }, []);
+
+  if (istryingLogin) {
+    return null;
+  }
+
+  return <Navigation />;
+}
+
 export default function App() {
   return (
     <>
       <StatusBar style="light" />
       <AuthContextProvider>
-        <Navigation />
+        <Root />
       </AuthContextProvider>
     </>
   );
