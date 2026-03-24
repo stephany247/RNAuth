f4c61bfd3a2e3e311af9ffb2d64e7fcd9cd9c3a2
Stephanie Oguocha <stephanieoguocha@gmail.com>
2026-03-24 09:45:54 +0100

    feat: store and manage auth state with React Context

---
+++ store/auth-context.js
@@ -0,0 +1,31 @@
+import { createContext, useState } from 'react';
+
+export const AuthContext = createContext({
+  token: '',
+  isAuthenticated: false,
+  authenticate: () => {},
+  logout: () => {},
+});
+
+function AuthContextProvider({ children }) {
+  const [authToken, setAuthToken] = useState();
+
+  function authenticate() {
+    setAuthToken(token);
+  }
+
+  function logOut() {
+    setAuthToken(null);
+  }
+
+  const value = {
+    token: authToken,
+    isAuthenticated: !!authToken,
+    authenticate: authenticate,
+    logOut: logOut,
+  };
+
+  return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
+}
+
+export default AuthContextProvider;
